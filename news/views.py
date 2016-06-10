df0ea31ae7cdaf509207a4741eac95d4e93d9002
Makrau <iago006@hotmail.com>
2016-06-09 22:37:51 -0300

    Mostrando as noticias na pagina inicial!

--- news/views.py
+++ news/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django import http
 from news.models import News
+from django.utils import timezone 
+
+
 # Create your views here.
 
 
@@ -9,3 +12,13 @@ def create_news(request):
 
 def show_news(request):
     return render(request, 'news/show_news.html', {})
+
+def home(request):
+    recent_news = News.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    
+    information = {
+    'news' : recent_news
+    }
+
+    return render(request, 'news/home.html', information)
+
