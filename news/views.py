b6b5661e486c816b3e8677e7c740319b2aedd279
Makrau <iago006@hotmail.com>
2016-06-14 15:05:22 -0300

    Mostrando a noticia na íntegra

--- news/views.py
+++ news/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django import http
 from news.models import News
 from django.utils import timezone 
+
 
 
 # Create your views here.
@@ -10,14 +11,21 @@ from django.utils import timezone
 def create_news(request):
     pass
 
-def show_news(request):
-    return render(request, 'news/show_news.html', {})
+def show_news(request, pk):
+    selected_news = get_object_or_404(News, pk=pk)
+    page_name = "Página de Notícia"
+    information = {
+        'selected_news' : selected_news,
+        'page_name' : page_name
+    }
+    return render(request, 'news/show_news.html', information)
 
 def home(request):
     recent_news = News.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    
+    page_name = "Página Inicial"
     information = {
-    'news' : recent_news
+        'news' : recent_news,
+        'page_name' : page_name
     }
 
     return render(request, 'news/home.html', information)
